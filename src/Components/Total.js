4ef84696008a804fed74306457903125a676ab6f
Soham Das <majumder70.ranu@gmail.com>
2022-01-10 10:03:45 +0530

    Display Cart Total and minor style fix

--- src/Components/Total.js
+++ src/Components/Total.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import CurrencyFormat from 'react-currency-format';
+import { getTotal } from '../reducer';
+import { useStateValue } from '../StateProvider';
 import './Total.css';
 
 function Total () {
+    const [ { cart }, dispatch ] = useStateValue();
     return(
         <div className='total'>
             <CurrencyFormat 
-                renderText={() => (
+                renderText={(value) => (
                 <>
                     <p>
-                    Subtotal (0 items): <strong>0</strong>
+                    Subtotal ({cart.length} items): <strong>{value}</strong>
                     </p>
                 </>
                 )}
                 decimalScale = {2}
-                value={0}
+                value={getTotal(cart)}
                 displayType={"text"}
                 thousandSeparator={true}
-                prefix={'Rs'}
+                prefix={'Rs '}
             />
             <button>Proceed to Checkout</button>
         </div>
