08d21e8a7d4f7ec8302009f6b3487e6131cb6a23
Soham Das <majumder70.ranu@gmail.com>
2022-01-15 21:32:37 +0530

    dummy review component

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -3,8 +3,20 @@ import './Review.css';
 
 export default function Review () {
     return (
-        <div className="review">
-            Product Review
-        </div>
+        <>
+            <h3 className="header">Review</h3>
+            <div className="review">
+                <div className="review__container">
+                    <h4>Good</h4>
+                    <p>
+                        Very good product
+                    </p>
+                </div>
+                <div className="review__container">
+                    <h4>Good</h4>
+                    <p>Very good product</p>
+                </div>
+            </div>
+        </>
     )
 }
