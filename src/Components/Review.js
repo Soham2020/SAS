d8d76043691d67b45854567334998aaf6f24c8ee
Soham Das <majumder70.ranu@gmail.com>
2022-01-21 21:29:06 +0530

    Feedback fetched

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import './Review.css';
 
 export default function Review () {
+    const [ feedback, setFeedback ] = useState([]);
+    const getFeedback = async() => {
+        const res = await axios.get('https://sas-rest-api.herokuapp.com/api/feedback')
+        setFeedback(res.data);
+    }
+    useEffect(() => {
+        getFeedback();
+    }, [])
     return (
         <>
-        {/* will add dynamics later */}
             <h3 className="header">Review</h3>
             <div className="review">
-                <div className="review__container">
-                    <h4>Good</h4>
-                    <p>
-                        Very good product
-                    </p>
-                </div>
+                {
+                    feedback.map((item) => {
+                        return (
+                            <div className="review__container">
+                                <h4>{item.name}</h4>
+                                <p>
+                                    {item.message}
+                                </p>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </>
     )
