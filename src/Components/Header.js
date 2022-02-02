b9c9754adc40d1acbceeaeb057d5612f9c7310ec
Soham Das <majumder70.ranu@gmail.com>
2022-02-02 19:16:20 +0530

    auth in view items

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,7 +15,7 @@ export default function Header () {
 
     const [ products, setProducts ] = useState([]);
     const getProducts = async() => {
-        const res = await axios.get('https://sas-rest-api.herokuapp.com/api/products')
+        const res = await axios.get('https://sas-rest-api.herokuapp.com/api/products', { headers: { token: token } })
         setProducts(res.data)
     }
     useEffect(() => {
