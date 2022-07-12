65e15416b87bd320e4b7a661265f82549300124d
Shoham Dey Sarkar <65813417+Shoham-87@users.noreply.github.com>
2022-07-12 22:29:58 +0530

    Update App.js
    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login';
@@ -6,8 +6,11 @@ import Register from './Components/Register';
 import Header from './Components/Header';
 import Cart from './Components/Cart';
 import SingleItem from './Components/SingleItem';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
+
   return (
     <div className="App">
       <Routes>
