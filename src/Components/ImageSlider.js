1cf6ebaebf553390767063c7d02427e5621afbbf
Shoham Dey Sarkar <65813417+Shoham-87@users.noreply.github.com>
2022-07-12 22:40:57 +0530

    Update ImageSlider.js

--- src/Components/ImageSlider.js
+++ src/Components/ImageSlider.js
@@ -1,47 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { SliderData } from './SliderData';
-import './ImageSlider.css'
+import { SliderData } from "./SliderData";
+import Carousel from "react-bootstrap/Carousel";
 
 function ImageSlider({ slides }) {
-    const [ current, setCurrent ] = useState(0);
-    const length = slides.length;
-
-    useEffect(() => {
-        if(current < 0)
-            setCurrent(length - 1);
-        if(current > length-1)
-            setCurrent(0);
-    }, [ current, length-1 ])
-
-    useEffect(() => {
-        let slider = setInterval(() => {
-            setCurrent(current + 1)
-        }, 4500);
-        return () => clearInterval(slider)
-    }, [current])
-
-    const nextSlide = () => {
-        setCurrent(current === length - 1 ? 0 : current + 1);
-    };
-
-    const prevSlide = () => {
-        setCurrent(current === 0 ? length - 1 : current - 1);
-    };
-
-    if (!Array.isArray(slides) || slides.length <= 0) {
-        return null;
-    }
   return (
     <>
-        {SliderData.map((slider, key) => {
-            return (
-                <div className={key === current ? 'slide active' : 'slide'} key={key}>
-                    {key === current && (
-                        <img src={slider.img} alt='travel image' className='logo' />
-                    )}
-                </div>
-            )
-        })}
+    <Carousel>
+
+      {SliderData.map((slider, key) => {
+        return (
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={slider.img}
+                alt="First slide"
+              />
+            </Carousel.Item>
+        );
+      })}
+    </Carousel>
     </>
   );
 }
