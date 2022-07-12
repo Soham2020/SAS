91c10dd2859674ea9d6e6883915d383de4cbf1ff
Shoham Dey Sarkar <65813417+Shoham-87@users.noreply.github.com>
2022-07-12 22:49:11 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchIcon from '@material-ui/icons/Search';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import { useNavigate, Link } from "react-router-dom";
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { useStateValue } from "../StateProvider";
 import './Header.css'
 
@@ -13,30 +14,78 @@ export default function Navbar(){
         return navigate('/');
     }
     return (
-        <div className="header">
-            <img 
-                className="header__logo"
-                src="https://s3.amazonaws.com/thumbnails.venngage.com/template/fc8535df-be09-4c80-8ea5-a69a34b2318e.png"
-            />
-            <div className="header__search">
-                <input 
-                    className="header__searchInput"
-                    type="text"
-                />
-                <SearchIcon className="header__searchIcon"/>
-            </div>
-            <div className="header__nav">
-                <div className="header__option">
-                    <span className="header__optionLineOne" onClick={logout}>Logout</span>
-                </div>
-                <div className="header__option">
-                    <Link to={"/user/cart"} style={{ textDecoration: 'none' }}><span className="header__optionLineTwo">Orders</span></Link>
-                </div>
-                <div className="header__optionBasket">
-                    <Link to={"/user/cart"} ><AddShoppingCartIcon /></Link>
-                    <span className="header__optionLineTwo header__basketCount">{ cart.length }</span>
-                </div>
-            </div>
-        </div>
-    )
-}
+        <>
+<nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light ">
+  <div className="container-fluid ">
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="fas fa-bars"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+      <a className="navbar-brand mt-2 mt-lg-0" href="/">
+        <img
+          src="https://s3.amazonaws.com/thumbnails.venngage.com/template/fc8535df-be09-4c80-8ea5-a69a34b2318e.png"
+          height="40"
+          alt="S-Mart"
+          loading="lazy"
+        />
+      </a>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link" href="/user/header">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/user/header">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/user/header">Products</a>
+        </li>
+      </ul>
+    </div>
+    <div className="d-flex align-items-center">
+    <span className="badge rounded-pill badge-notification bg-danger">{ cart.length }</span>
+    <Link to={"/user/cart"} className="me-3"><AddShoppingCartIcon /></Link>
+      <div className="dropdown">
+        <a
+          className="dropdown-toggle d-flex align-items-center hidden-arrow"
+          href="#"
+          id="navbarDropdownMenuAvatar"
+          role="button"
+          data-toggle="dropdown"
+          aria-expanded="false"
+        >
+        <AccountCircleIcon className="rounded-circle"/>
+        </a>
+        {/* <ul
+          className="dropdown-menu dropdown-menu-end"
+          aria-labelledby="navbarDropdownMenuAvatar"
+        >
+          <li>
+            <a className="dropdown-item" href="#">My profile</a>
+          </li>
+          <li>
+            <a className="dropdown-item" href="#" onClick={logout}>Logout</a>
+          </li>
+        </ul> */}
+      </div>
+      <ul className="navbar-nav me-3 mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link cursor-pointer" href="#" onClick={logout}>Log Out</a>
+        </li>
+        </ul>
+    </div>
+  </div>
+</nav>
+<div style={{marginBottom:"3.8em"}}></div>
+        </>
+        )
+    }
